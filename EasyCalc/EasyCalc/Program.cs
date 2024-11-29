e96c44f000d0e5f06460ff5fe762c0ccbcd95485
denGdprod <dengoodpro@gmail.com>
2024-11-29 04:01:13 +0300

    Создан простейший калькулятор

--- EasyCalc/EasyCalc/Program.cs
+++ EasyCalc/EasyCalc/Program.cs
@@ -6,7 +6,38 @@ namespace EasyCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Простой калькулятор");
+            Console.WriteLine("Введите первое число:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор (+, -, *, /):");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    result = number1 / number2;
+                    break;
+                default:
+                    Console.WriteLine("Неверный оператор");
+                    return;
+            }
+
+            Console.WriteLine($"Результат: {number1} {operation} {number2} = {result}");
         }
     }
 }
